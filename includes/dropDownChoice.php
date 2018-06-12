2c60807ff83a1aad421981ab9588144fd3d82449
Plamen Georgiev <p.olegov@yahoo.com>
2018-06-12 22:03:26 +0300

    Нови опции за отговор

--- includes/dropDownChoice.php
+++ includes/dropDownChoice.php
@@ -13,6 +13,14 @@ while ($row = $result->fetch_assoc()) {
         $checkBox = $row['input_type_name'];
     } else if ($row['id'] == 3) {
         $radio = $row['input_type_name'];
+    } else if ($row['id']==4){
+        $scale = $row['input_type_name'];
+    }else if ($row['id']==5){
+        $picture = $row['input_type_name'];
+    }else if ($row['id']==6){
+        $sound = $row['input_type_name'];
+    }else if ($row['id']==7){
+        $video = $row['input_type_name'];
     }
 }
 echo '<ul class="dropdown-menu" >';
@@ -22,6 +30,14 @@ echo '<li ><a href = "#" class="glyphicon glyphicon-check" id = "addCheckBox"
         onclick = "for (var i = 0; i <= 2; i++){createCheckBox();i++;}; document.forms[0].elements[\'qtype\'].value = 2" > -' . $checkBox . '</a ></li >';
 echo '<li ><a href = "#" class="glyphicon glyphicon-screenshot" 
         onclick = "for (var i = 0; i <= 2; i++){createRadio();i++; }; document.forms[0].elements[\'qtype\'].value = 3" " > -' . $radio . '</a ></li >';
+echo '<li ><a href = "#" class="glyphicon glyphicon-resize-horizontal" id = "addScale"
+        onclick = "document.forms[0].elements[\'qtype\'].value = 4" " > -' . $scale. '</a ></li>';
+echo '<li ><a href = "#" class="glyphicon glyphicon-picture" id = "addScale"
+        onclick = "document.forms[0].elements[\'qtype\'].value = 5" " > -' . $picture. '</a ></li>';
+echo '<li ><a href = "#" class="glyphicon glyphicon-sound-stereo" id = "addScale"
+        onclick = "document.forms[0].elements[\'qtype\'].value = 6" " > -' . $sound. '</a ></li>';
+echo '<li ><a href = "#" class="glyphicon glyphicon-hd-video" id = "addScale"
+        onclick = "document.forms[0].elements[\'qtype\'].value = 7" " > -' . $video. '</a ></li>';
 echo '</ul >';
 $result->free();
 ?>
